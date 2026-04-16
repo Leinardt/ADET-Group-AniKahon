3704bc5d60d6a20b9840f311378f81226493ae09
Leinardt <leinardt001@gmail.com>
2026-04-17 01:10:02 +0800

    Update service.php

--- src/service.php
+++ src/service.php
@@ -1,5 +1,14 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
+
+// Handle preflight request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 // GET JSON INPUT
 $data = json_decode(file_get_contents("php://input"), true);
