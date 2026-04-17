42898825f0d36a7bf6ca995a48a3d5970892ee31
Leinardt <leinardt001@gmail.com>
2026-04-17 14:59:57 +0800

    Update adet_db.sql

--- src/adet_db.sql
+++ src/adet_db.sql
@@ -9,7 +9,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
   `full_name` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `phone` varchar(20) DEFAULT NULL,
   `address` text DEFAULT NULL,
@@ -18,16 +18,16 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
-INSERT INTO `users` (`user_id`, `full_name`, `email`, `password`, `phone`, `address`, `role`, `created_at`) VALUES
-(6, 'Leinardt Omadto', 'leinardt@anikahon.com', 'password123', '09123456781', 'Legazpi City, Albay', 'user', '2026-04-17 04:19:29'),
-(7, 'Nyan Alcovindas', 'nyan@anikahon.com', 'password123', '09123456782', 'Legazpi City, Albay', 'user', '2026-04-17 04:19:29'),
-(8, 'Lance Christopher Delos Reyes', 'lance@anikahon.com', 'password123', '09123456783', 'Daraga, Albay', 'user', '2026-04-17 04:19:29'),
-(9, 'Rhona Mae Pancho', 'rhona@anikahon.com', 'password123', '09123456784', 'Legazpi City, Albay', 'admin', '2026-04-17 04:19:29');
+INSERT INTO `users` (`user_id`, `full_name`, `username`, `password`, `phone`, `address`, `role`, `created_at`) VALUES
+(6, 'Leinardt Omadto', 'leinardt', 'password123', '09123456781', 'Legazpi City, Albay', 'user', '2026-04-17 04:19:29'),
+(7, 'Nyan Alcovindas', 'nyan', 'password123', '09123456782', 'Legazpi City, Albay', 'user', '2026-04-17 04:19:29'),
+(8, 'Lance Christopher Delos Reyes', 'lance', 'password123', '09123456783', 'Daraga, Albay', 'user', '2026-04-17 04:19:29'),
+(9, 'Rhona Mae Pancho', 'rhona', 'password123', '09123456784', 'Legazpi City, Albay', 'admin', '2026-04-17 04:19:29');
 
 
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD UNIQUE KEY `username` (`username`);
 
 
 ALTER TABLE `users`
